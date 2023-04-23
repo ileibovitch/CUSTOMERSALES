59efe15bfe7d5a34f46bf0b4f9ec6ded5161c550
win7 <win7@BDL.lan>
2023-04-23 12:41:30 +0200

    adding more unit tests

--- CustomerService/src/test/java/com/poalim/customersales/controller/CustomerServiceTest.java
+++ CustomerService/src/test/java/com/poalim/customersales/controller/CustomerServiceTest.java
@@ -64,4 +64,57 @@ public class CustomerServiceTest {
 
         Assertions.assertFalse(result.isBlocked());
     }
+    
+    @Test
+    public void testAddProposal() {
+        // Given
+        Long id = 1234L;
+        Customer mockedCustomer = new Customer();
+        mockedCustomer.setCustomerId(id);
+        mockedCustomer.setName("CustomerName");
+        mockedCustomer.setBlocked(false);
+        mockedCustomer.setProposalsPerDay(0);
+        Mockito.when(customerRepository.findById(id)).thenReturn(Optional.of(mockedCustomer));
+        Mockito.when(customerMap.get(anyLong())).thenReturn(mockedCustomer);
+        Mockito.when(customerMap.containsKey(anyLong())).thenReturn(true);
+
+        // When
+        Customer result = customerService.addProposal(id);
+
+        Assertions.assertEquals(result.getProposalsPerDay(), 1);
+    }
+    
+    @Test
+    public void testRemoveProposal() {
+        // Given
+        Long id = 1234L;
+        Customer mockedCustomer = new Customer();
+        mockedCustomer.setCustomerId(id);
+        mockedCustomer.setName("CustomerName");
+        mockedCustomer.setBlocked(false);
+        mockedCustomer.setProposalsPerDay(1);
+        Mockito.when(customerRepository.findById(id)).thenReturn(Optional.of(mockedCustomer));
+        Mockito.when(customerMap.get(anyLong())).thenReturn(mockedCustomer);
+        Mockito.when(customerMap.containsKey(anyLong())).thenReturn(true);
+
+        // When
+        Customer result = customerService.removeProposal(id);
+
+        Assertions.assertEquals(result.getProposalsPerDay(), 0);
+    }
+    
+    @Test
+    public void testUnblockCustomerCustomerMapEmptyRuntimeExceptionThrown() {
+        // Arrange
+        Long customerId = 1234L;
+        Mockito.when(customerMap.containsKey(customerId)).thenReturn(false); // Mocking customerMap to be empty
+
+        // Act
+        Assertions.assertThrows(RuntimeException.class, () -> customerService.addProposal(customerId));
+        Assertions.assertThrows(RuntimeException.class, () -> customerService.removeProposal(customerId));
+        Assertions.assertThrows(RuntimeException.class, () -> customerService.blockCustomer(customerId)); 
+        Assertions.assertThrows(RuntimeException.class, () -> customerService.unblockCustomer(customerId));
+
+        
+    }
 }
